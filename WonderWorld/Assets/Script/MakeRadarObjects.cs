8aa24b477fd8693cefd2b6ffe9c3bd19e7be6582
starboy007-debug <65112799+starboy007-debug@users.noreply.github.com>
2020-12-22 21:21:31 +0530

    fixing light

---
+++ WonderWorld/Assets/Script/MakeRadarObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeRadarObjects : MonoBehaviour
+{
+    public Image image;
+    void Start()
+    {
+        Radar.RegisterRadarObject(this.gameObject, image);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        Radar.RemoveRadarObject(this.gameObject);
+    }
+
+}
---
+++ WonderWorld/Assets/Script/Radar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.Experimental.UIElements;
+
+public class RadarObject
+{
+    public Image icon { get; set; }
+    public GameObject owner { get; set; }
+
+}
+
+public class Radar : MonoBehaviour
+{
+    public Transform playerPos;
+    float mapScale = 2.0f;
+
+    public static List<RadarObject> radObjects = new List<RadarObject>();
+
+    public static void RegisterRadarObject(GameObject o, Image i)
+    {
+        Image image = Instantiate(i);
+        radObjects.Add(new RadarObject() { owner = o, icon = image });
+    }
+
+    public static void RemoveRadarObject(GameObject o)
+    {
+        List<RadarObject> newList = new List<RadarObject>();
+        for (int i = 0; i < radObjects.Count; i++)
+        {
+            if (radObjects[i].owner == o)
+            {
+                Destroy(radObjects[i].icon);
+                continue;
+            }
+            else
+                newList.Add(radObjects[i]);
+        }
+
+        radObjects.RemoveRange(0, radObjects.Count);
+        radObjects.AddRange(newList);
+    }
+
+    void DrawRadarDots()
+    {
+        foreach (RadarObject ro in radObjects)
+        {
+            Vector3 radarPos = (ro.owner.transform.position - playerPos.position);
+            float distToObject = Vector3.Distance(playerPos.position, ro.owner.transform.position) * mapScale;
+            float delay = Mathf.Atan2(radarPos.x, radarPos.z) * Mathf.Rad2Deg - 270 - playerPos.eulerAngles.y;
+            radarPos.x = distToObject * Mathf.Cos(delay * Mathf.Deg2Rad) * -1;
+            radarPos.z = distToObject * Mathf.Sin(delay * Mathf.Deg2Rad);
+
+            ro.icon.transform.SetParent(this.transform);
+            ro.icon.transform.position = new Vector3(radarPos.x, radarPos.z, 0) + this.transform.position;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        DrawRadarDots();
+    }
+}
